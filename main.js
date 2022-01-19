b3877dcb304fa719296e3eb6f2d6e97356718817
elpbatista <elpbatista@gmail.com>
2022-01-18 18:36:15 -0800

    density color scale

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 window.onload = () => {
+  const log = true;
   const baseURL =
     "https://raw.githubusercontent.com/digital-guard/preservCutGeo-BR2021/main/data/MG/BeloHorizonte/_pk0008.01/geoaddress/";
   const fp_ghs = "geohahes";
   const map = L.map("map", {});
+  const colors = chroma.scale("YlGnBu");
+  const normalize = (val, max, min) => (val - min) / (max - min);
 
   const tiles = L.tileLayer(
     "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw",
@@ -23,12 +26,9 @@ window.onload = () => {
   //     fillColor: "#ff0000",
   //   });
   // });
-  
+
   function onEachFeature(feature, layer) {
-    let popupContent =
-      "type: " +
-      feature.type +
-      "</p>";
+    let popupContent = "type: " + feature.type + "</p>";
     if (feature.properties && feature.properties.ghs) {
       popupContent += feature.properties.val_density_km2;
     }
@@ -42,9 +42,54 @@ window.onload = () => {
       return response.json();
     })
     .then(function (data) {
-      console.log(data.features);
+      let densities = [
+        ...new Set(
+          data.features.map((a) => Math.round(a.properties.val_density_km2))
+        ),
+      ];
+      // ].sort((a, b) => a - b);
+      let max = Math.max(...densities);
+      let min = Math.min(...densities);
+      
+      // +++++++++++++++++++++++++++++++++++++++++++++++++++
+      if (log) {
+        console.log(chroma.brewer.YlGnBu); //9  colors
+        console.log(densities);
+        console.log(
+          colors(
+            normalize(
+              Math.round(data.features[2].properties.val_density_km2),
+              max,
+              min
+            )
+          ).hex()
+        );
+        console.log(min);
+        console.log(max);
+        console.log(normalize(300, max, min));
+        console.log(Math.round(data.features[2].properties.val_density_km2));
+      };
+      // +++++++++++++++++++++++++++++++++++++++++++++++++++
+
       geohashes = L.geoJSON(data, {
         onEachFeature: onEachFeature,
+        style: function (feature) {
+          console.log(feature);
+          return {
+            fillColor: colors(
+              normalize(
+                // Math.round(data.features[300].properties.val_density_km2),
+                Math.round(feature.geometry.properties.val_density_km2),
+                max,
+                min
+              )
+            ).hex(),
+            color: "#000",
+            weight: .125,
+            // opacity: 1,
+            fillOpacity: .65,
+          };
+        },
       }).addTo(map);
       map.fitBounds(geohashes.getBounds());
     });
